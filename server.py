6c0c8b7577b06b38e6e84b3bcc07f1dd1298dc87
Fremah Agyemang <captainfremah66@gmail.com>
2023-07-03 02:25:22 +0000

    edit sever

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ listen_socket.listen(1) # listen for connections
 print('Serving HTTP on port {PORT} ...')
 while True:
     client_connection, client_address = listen_socket.accept()  #accept and return new socket and bind address
-    print("Fremah")
     request_data = client_connection.recv(1024)  #returns bytes. argument is maximum amount of data to be received
     print(request_data.decode('utf-8')) #decode to avoid problems with data
 
@@ -18,7 +17,6 @@ HTTP/1.1 200 OK
 
 Hello, World, I just created a Web Server...!
 """
-    print(http_response)
     client_connection.sendall(http_response) # send response
     client_connection.close() # close connection
     
